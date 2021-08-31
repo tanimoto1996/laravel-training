24d9b3b72dab09bd573c9b53af94b218e3c99d85
tanimoto <yumenikki2@gmail.com>
2021-08-31 11:08:05 +0900

    モデルにfillableの設定、ファクトリー・シーダの設定。

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // POSTシーダを実行する
+        $this->call(PostSeeder::class);
     }
 }
